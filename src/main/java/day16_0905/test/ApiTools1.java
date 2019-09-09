2cf66288df8094de169573b1b5e88adfc6bce881
zswzaq <342555822@qq.com>
2019-09-09 22:29:12 +0800

    qwq

--- src/main/java/day16_0905/test/ApiTools1.java
+++ src/main/java/day16_0905/test/ApiTools1.java
@@ -3,9 +3,9 @@ package day16_0905.test;
 /**
  * 接口工具类
  */
-public class ApiTools {
+public class ApiTools1 {
     // 为数据提供者
     public static Object[][] getData() {
-
+        return null;
     }
 }
